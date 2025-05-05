f10b48284c1109403313499e2fbb1f622ea5bcc8
estefany-123 <estefany_parada@soy.sena.edu.co>
2025-05-04 21:59:19 -0500

    La seed imprime en consola todos los pokemons, iniciando creación masiva

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'This action returns all seed';
+
+  private readonly axios : AxiosInstance = axios;
+
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    data.results.forEach(({name,url}) => {
+      const segment = url.split('/');
+      const no: number = +segment[ segment.length - 2 ];
+
+      console.log(`El pokemon ${name} tiene id ${no}`);
+    })
+    return data.results;
   }
 }
